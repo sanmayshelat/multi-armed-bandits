670f7185fe17d352fb5f3f63d01c59359691f89e
Sanmay Shelat <sanmay.shelat@free-now.com>
2023-09-28 15:35:48 -0400

    now using scipy rv_frozen for thompson sampling, added gaussian priors, added additional validation

--- mab_algo.py
+++ mab_algo.py
@@ -1,27 +1,30 @@
-import numpy as np
-from bandit_dgp import BanditDGP
 from enum import Enum
-from pydantic import BaseModel
-from scipy.stats import beta
-from typing import List, Union
+from typing import List, Union, Optional
 
-class GreedyMethodsEnum(Enum):
+import numpy as np
+from pydantic import BaseModel, ValidationError, validator
+from scipy.stats._distn_infrastructure import rv_continuous_frozen
+from scipy.stats import beta, norm
+
+from bandit_dgp import BanditDGPBernoulli, BanditDGPGaussian
+
+class GreedyMethods(Enum):
     greedy='greedy'
     optimistic_init='optimistic_init'
     ucb1='ucb1'
     thompson_sampling='thompson_sampling'
 
 class GreedyMethodsInput(BaseModel):
-    method: GreedyMethodsEnum = GreedyMethodsEnum.greedy
-
+    method: GreedyMethods = GreedyMethods.greedy
 
 class MAB():
 
     def __init__(
         self,
-        dgp: BanditDGP,
+        dgp: Union[BanditDGPBernoulli, BanditDGPGaussian],
         pulls: int,
-        seed: int=42
+        seed: int=42,
+        known_std: Optional[float]=None
         ):
 
         self.dgp = dgp
@@ -33,35 +36,77 @@ class MAB():
         self.perf_avg = np.empty(self.n_arms) * np.nan
         self.num_samples = np.zeros(self.n_arms, dtype=int)
         self.last_sample = np.zeros(self.n_arms) * np.nan
+
+        self.known_std = known_std
     
     def pull_dgp_arm(self, arm_to_pull):
         arm_outcome_index = self.num_samples[arm_to_pull]
         return self.outcomes[arm_to_pull, arm_outcome_index]
 
-    
-    def pull_all_arms_first(self, optimistic_init: bool=False):
+    def pull_all_arms_first(self, method):
         self.reward = []
         for i in range(self.n_arms):
-            self.last_sample[i] = self.pull_dgp_arm(i) * (not optimistic_init) + 0.99 * (optimistic_init)
+            self.last_sample[i] = self.pull_dgp_arm(i)
+            
+            if method=='optimistic_init':
+                self.last_sample[i] += 0.99
+            
             self.num_samples[i] = 1
             self.perf_avg[i] = self.last_sample[i]
             self.reward += [self.outcomes[i,0]]
+            
+            if method=='thompson_sampling':
+                self.priors[i] = self.update_prior(
+                        prior=self.priors[i], 
+                        x_n=self.last_sample[i]
+                    )
+                print(i, self.last_sample[i], [k.args[0] for k in self.priors])
+            
         
     def update_perf_avg(self, x_bar_n_1, x_n, n):
-        print(x_n, x_bar_n_1, n)
         return (x_bar_n_1 + (x_n - x_bar_n_1)/n)
+    
+    def update_prior(self, prior, x_n):
+        if prior.dist.name=='beta':
+            updated_prior_args = tuple(np.array(prior.args) + np.array([x_n, 1-x_n]))
+            updated_prior = beta(updated_prior_args[0], updated_prior_args[1])
+        elif prior.dist.name=='norm':
+            if not self.known_std:
+                raise ValueError("The known std must be provided for this distribution.")
+            prior_mean = prior.args[0]
+            prior_var = prior.args[1]**2
+            posterior_var = 1/(1/prior_var + 1/self.known_std)
+            posterior_mean = posterior_var * (prior_mean/prior_var + x_n/self.known_std)
+            updated_prior_args = tuple([posterior_mean, np.sqrt(posterior_var)])
+            updated_prior = norm(updated_prior_args[0], updated_prior_args[1])
+        else:
+            raise ValueError("The posterior of this distribution is not available.")
+        return updated_prior
 
-    def greedy(self, method: str='greedy', priors: np.array=None):
-        # validate method
+    def validate_greedy_methods(self, method):
         GreedyMethodsInput(method=method)
+        if isinstance(self.dgp, BanditDGPGaussian) and method=='optimistic_init':
+            raise NotImplementedError('Using optimistic_init for Gaussian rewards is not available yet')    
+
+    def validate_priors(self, method, priors):
         if method=='thompson_sampling':
             if not priors:
-                self.priors=np.ones((self.n_arms,2))
+                raise ValueError('Priors have to be supplied for thompson_sampling')
+            elif isinstance(priors, np.ndarray):
+                if all(isinstance(i, rv_continuous_frozen) for i in priors):
+                    self.priors = priors
+            elif isinstance(priors, rv_continuous_frozen):
+                self.priors = np.repeat(priors, self.n_arms)
             else:
-                self.priors=priors
+                raise ValueError('Priors have to be scipy.stats distributions')  
+
+    def greedy(self, method: str='greedy', priors: np.ndarray=None):
+        # validate inputs
+        self.validate_greedy_methods(method=method)
+        self.validate_priors(method=method, priors=priors)
         
         # pull all arms first
-        self.pull_all_arms_first(optimistic_init= method=='optimistic_init')
+        self.pull_all_arms_first(method=method)
 
         while sum(self.num_samples) < self.pulls:
             if method in ['greedy', 'optimistic_init']:
@@ -76,23 +121,30 @@ class MAB():
                 )            
                 print(best_perf_arm, self.perf_ucb1)
             elif method=='thompson_sampling':
-                self.perf_sample = self.rng.beta(self.priors[:,0], self.priors[:,1])
+                self.perf_sample = np.array([i.rvs() for i in self.priors])
                 best_perf_arm = self.rng.choice(
                     np.where(self.perf_sample==self.perf_sample.max())[0]
                 )
+                # print(best_perf_arm, self.perf_sample)
 
             self.last_sample[best_perf_arm] = self.pull_dgp_arm(best_perf_arm)
             self.reward += [self.last_sample[best_perf_arm]]
             self.num_samples[best_perf_arm] += 1
+
             new_perf_avg = self.update_perf_avg(
                 x_bar_n_1=self.perf_avg[best_perf_arm],
                 x_n=self.last_sample[best_perf_arm],
                 n=self.num_samples[best_perf_arm]
             )
             self.perf_avg[best_perf_arm] = new_perf_avg
+
             if method=='thompson_sampling':
-                self.priors[best_perf_arm,0] += self.last_sample[best_perf_arm]
-                self.priors[best_perf_arm,1] += 1-self.last_sample[best_perf_arm]
+                self.priors[best_perf_arm] = self.update_prior(
+                    prior=self.priors[best_perf_arm], 
+                    x_n=self.last_sample[best_perf_arm]
+                )
+                print(best_perf_arm, self.perf_sample,self.last_sample[best_perf_arm], [i.args[0] for i in self.priors])
+            
 
     def epsilon_greedy(self, epsilon: int=0.1, decay_rate: int=None):
         self.pull_all_arms_first()
@@ -117,10 +169,13 @@ class MAB():
             self.perf_avg[arm_to_pull] = new_perf_avg
 
 
-bandits = BanditDGP(prob_success=[0.2,0.5,0.75])
-mab = MAB(dgp=bandits, pulls=2000)
+# bandits = BanditDGPBernoulli(prob_success=[0.2,0.5,0.75])
+bandits = BanditDGPGaussian([5,10,20], 1)
+mab = MAB(dgp=bandits, pulls=2000, known_std=1)
 # mab.epsilon_greedy(epsilon=0.999999)
-mab.greedy(method='thompson_sampling')
-print(mab.priors, sum(mab.reward))
+mab.greedy(method='thompson_sampling', priors=norm(10,1))
+# mab.greedy()
+print(mab.perf_avg, sum(mab.reward))
+print([i.args for i in mab.priors])
 
 
